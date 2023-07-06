9bda8f46a4b76feddd7ed2e8e475ad1d09b91d2d
addoumi mohammed <addoumi.mohammed@gmail.com>
2023-07-06 12:33:35 +0100

    init commit

---
+++ src/main/java/com/kafkastreams/kafkastreamsproducer/KafkaStreamsProducerApplication.java
@@ -0,0 +1,13 @@
+package com.kafkastreams.kafkastreamsproducer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaStreamsProducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaStreamsProducerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kafkastreams/kafkastreamsproducer/KafkaStreamsProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kafkastreams.kafkastreamsproducer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaStreamsProducerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
